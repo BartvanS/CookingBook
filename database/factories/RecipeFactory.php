39970588cf8f4b3f07b10249411c165cb4806646
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2020-09-29 17:44:41 +0200

    added non working crud recipes with db side

---
+++ database/factories/RecipeFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Recipe;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class RecipeFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Recipe::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->domainName,
+            'description' => $this->faker->realText(200),
+            'hours' => rand(1, 3),
+            'minutes' => rand(1, 59),
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Recipe;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,14 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'bart',
+            'email' => 'bart@bart.nl',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
+        Recipe::factory(50)->create();
     }
 }
