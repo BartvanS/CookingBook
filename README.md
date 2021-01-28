58813c0f2714b3944a89da92a5f452431ec33915
Bart van Sevenbergen <bartvansevenbergen@hotmail.com>
2021-01-29 00:09:53 +0100

    create page use textarea component

--- README.md
+++ README.md
@@ -3,3 +3,13 @@
 This is very basic crud recipe website to learn the basics of laravel 8
 
 In dit website you can manage recipes as you please.
+
+
+art = php artisan
+step 1: setup .env file
+step 2: npm i
+step 3: art migrate:fresh --seed
+
+elke keer voor gebruik terminals starten met:
+npm run watch
+php artisan serve (art serve)
--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -7,46 +7,28 @@
 
     <form method="post" action="{{route('recipes.store')}}">
         @csrf
-
         <div class="bg-white rounded-lg container mx-auto max-w-md relative mt-12">
-
             <div class="font-bold text-xl bg-white rounded-full px-3 py-2 absolute ml-5 shadow"
                  style="margin-top: -25px">
                 Nieuw recept toevoegen
             </div>
-
             <div class="flex flex-col p-5 pt-8">
-
                 <x-input
                     type="text"
                     id="title"
                     class="px-3 py-2 rounded-lg border border-gray-300"
                     label="Titel"
                 />
-
-                <label for="description" class="mb-1 mt-3">Beschrijving</label>
-                <textarea class="autoResizeTextArea px-3 py-2 rounded-lg border border-gray-300"
-                          type="text"
-                          name="description"
-                          id="description">{{ old('description') }}</textarea>
-                @error('description')
-                <div class="text-red-800 mt-1">
-                    {{ $message }}
-                </div>
-                @enderror
-
-                <label for="ingredients" class="mb-1 mt-3">Ingredienten</label>
-                <textarea class="autoResizeTextArea px-3 py-2 rounded-lg border border-gray-300"
-                          type="text"
-                          name="ingredients"
-                          id="ingredients"
-                          value="{{old('ingredients')}}"></textarea>
-                @error('ingredients')
-                <div class="text-red-800 mt-1">
-                    {{ $message }}
-                </div>
-                @enderror
-
+                <x-text-area
+                    id="description"
+                    class="autoResizeTextArea px-3 py-2 rounded-lg border border-gray-300"
+                    label="Beschrijving"
+                />
+                <x-text-area
+                    id="ingredients"
+                    class="autoResizeTextArea px-3 py-2 rounded-lg border border-gray-300"
+                    label="Ingredienten"
+                />
                 <div class="flex mt-3">
                     <div class="flex-grow flex-col">
                         <x-input
@@ -56,7 +38,6 @@
                             label="Tijd in uren"
                         />
                     </div>
-
                     <div class="flex-grow flex-col ml-3">
                         <div class="flex-grow flex-col">
                             <x-input
@@ -68,12 +49,10 @@
                         </div>
                     </div>
                 </div>
-
                 <input type="submit"
                        class="px-3 py-2 rounded-lg bg-blue-600 text-white font-bold text-xl mt-5 hover:bg-blue-800 transition transition-colors duration-100"
                        value="Toevoegen">
             </div>
-
         </div>
     </form>
 </x-app-layout>
