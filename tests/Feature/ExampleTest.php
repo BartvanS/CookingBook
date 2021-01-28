e22eeb13be186f3bd02986a7c466c76ac9ce261f
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-01-28 23:44:46 +0100

    Fixed tests

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,20 +2,12 @@
 
 namespace Tests\Feature;
 
-use App\Models\User;
 use Tests\TestCase;
 
 class ExampleTest extends TestCase
 {
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
     public function testBasicTest()
     {
-        $this->actingAs(User::factory()->create());
-
         $response = $this->get('/');
 
         $response->assertRedirect('/recipes');
