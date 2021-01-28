c412bc93dad5977bf3a42944c907312f041cad74
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-01-28 17:43:55 +0100

    Improved recept create button

--- resources/views/recipes/index.blade.php
+++ resources/views/recipes/index.blade.php
@@ -1,11 +1,16 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-        <a href="{{route('recipes.create')}}">creeer</a>
-    </x-slot>
+        <div class="flex justify-between items-center">
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                {{ __('Recepten') }}
+            </h2>
 
+            <a href="{{route('recipes.create')}}"
+               class="px-3 py-1 rounded-lg bg-blue-600 text-white font-bold hover:bg-blue-800 transition transition-colors duration-100">
+                Nieuwe
+            </a>
+        </div>
+    </x-slot>
 
     <livewire:recipes likeQuery=""/>
 
