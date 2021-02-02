051de03bae5376b6489124effbb1c8dbf1e78974
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-02 13:14:25 +0100

    Fixed JS assets

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,7 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .js('resources/js/autoResizeTextArea.js', 'public/js')
+    .version()
     .postCss('resources/css/app.css', 'public/css', [
         require('postcss-import'),
         require('tailwindcss'),
