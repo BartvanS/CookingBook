c20d63846e93a7a1551dcd6b52d234d7a66089ee
BartvanS <bartvansevenbergen@hotmail.com>
2021-02-05 15:23:08 +0100

    recipeRepository phpdocs

--- app/Repositories/RecipeRepository.php
+++ app/Repositories/RecipeRepository.php
@@ -8,6 +8,13 @@ use App\Models\Recipe;
 
 final class RecipeRepository
 {
+
+    /**
+     * store a new recipe in the database based on the 
+     *
+     * @param  array $validatedValues
+     * @return Recipe $recipe
+     */
     public function store($validatedValues)
     {
         $recipe = new Recipe();
@@ -21,13 +28,18 @@ final class RecipeRepository
         return $recipe;
     }
 
+    /**
+     * update a recipe in the database
+     *
+     * @param  Recipe $recipe
+     * @param  array $validatedValues
+     * @return Recipe $recipe
+     */
     public function update($recipe, $validatedValues)
     {
         $recipe->update($validatedValues);
-
         $recipe->ingredients()->delete();
         $recipe->ingredients()->saveMany($validatedValues['ingredients']);
-
         $recipe->instructions()->delete();
         $recipe->instructions()->saveMany($validatedValues['instructions']);
 
