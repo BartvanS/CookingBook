92743ec533352725424dc5d88187bf012ac33927
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-05 17:25:46 +0100

    Some translations!

--- resources/views/recipes/create.blade.php
+++ resources/views/recipes/create.blade.php
@@ -5,98 +5,37 @@
         </h2>
     </x-slot>
 
-    <x-form action="{{route('recipes.store')}}" title="Nieuw recept">
-        <x-input type="text"
-                 id="title"
-                 label="Titel"/>
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 flex flex-col">
 
-        <x-text-area name="description"
-                     label="Beschrijving"/>
+        <x-form action="{{ route('recipes.store') }}"
+                title="{{ __('New recipe') }}">
 
-        <x-category-select id="category"
-                           label="Category"/>
+            <x-input name="title"
+                     label="{{ __('Title') }}"/>
 
-        <livewire:list-input name="ingredients"
-                             label="Ingredients"/>
+            <x-text-area name="description"
+                         label="{{ __('Description') }}"/>
 
-        <livewire:list-input name="instructions"
-                             label="Instructions"/>
+            <x-category-select id="category"
+                               label="{{ __('Category') }}"/>
 
-        {{--Time--}}
-        <x-input type="time"
-                 id="duration"
-                 label="Bereidingstijd"
-                 min="0"
-        />
+            <livewire:list-input name="ingredients"
+                                 label="{{ __('Ingredients') }}"/>
 
-        {{--submit--}}
-        <input
-            type="submit"
-            class="px-3 py-2 rounded-lg bg-blue-600 text-white font-bold text-xl mt-5 hover:bg-blue-800 transition transition-colors duration-100"
-            value="Toevoegen"
-        />
-    </x-form>
+            <livewire:list-input name="instructions"
+                                 label="{{ __('Instructions') }}"/>
+
+            <x-input type="time"
+                     name="duration"
+                     label="{{ __('Cooking time') }}"
+                     min="0"
+            />
+
+            <input
+                type="submit"
+                class="px-3 py-2 rounded-lg bg-blue-600 text-white font-bold text-xl mt-5 hover:bg-blue-800 transition transition-colors duration-100"
+                value="{{ __('Create') }}"
+            />
+        </x-form>
+    </div>
 </x-app-layout>
-
-<script>
-    let hiddenIngredientFieldEl = document.getElementById("ingredients");
-    let ingredientInputEl = document.getElementById("ingredientInput");
-    ingredientInputEl.addEventListener("keydown", function (event) {
-        if (event.ctrlKey && event.key === "Enter") {
-            //rework me or something
-        } else if (event.key === "Enter") {
-            event.preventDefault();
-            addToIngredientsList();
-        }
-    });
-
-    // let parsedIngredients = JSON.parse(hiddenIngredientFieldEl.value);
-    if (hiddenIngredientFieldEl.value.length <= 0) {
-        hiddenIngredientFieldEl.value = '[]';
-    }
-    let ingredients = JSON.parse(hiddenIngredientFieldEl.value);
-
-    ingredients.forEach(element => addToIngredientsListHtml(element));
-
-    function validateSubmit() {
-        let confirmContinue = confirm("Wil je dit opslaan?");
-        if (confirmContinue) {
-            //disable to not send the data
-            ingredientInputEl.setAttribute("disabled", "disabled");
-        }
-        return confirmContinue;
-    }
-
-    function addToIngredientsList() {
-        let inputValue = ingredientInputEl.value;
-        if (!inputValue || !ingredients.indexOf(inputValue)) {
-            //todo: error box
-            console.log("geen waarde voor ingredient")
-            return;
-        }
-        ingredients.push(inputValue)
-        ingredientInputEl.value = "";
-        ingredientInputEl.focus();
-        hiddenIngredientFieldEl.value = JSON.stringify(ingredients);
-        addToIngredientsListHtml(inputValue);
-    }
-
-    function addToIngredientsListHtml(inputValue) {
-        document.getElementById("ingredientList").innerHTML +=
-            '<div class="flex" id="ingredient-' + inputValue + '">' +
-            '<div class="flex-1">' + inputValue + '</div>' +
-            '<button class="flex-4" type="button" onclick="removeFromIngredientsList(this)" >' +
-            '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke="currentColor" width="20px">' +
-            '<path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />' +
-            '</svg>' +
-            '</button>' +
-            '</div>';
-    }
-
-    function removeFromIngredientsList(elem) {
-        ingredients = ingredients.filter(item => "ingredient-" + item !== elem.parentNode.id);
-        elem.parentNode.parentNode.removeChild(elem.parentNode);
-        hiddenIngredientFieldEl.value = JSON.stringify(ingredients);
-    }
-
-</script>
--- resources/views/recipes/show.blade.php
+++ resources/views/recipes/show.blade.php
@@ -1,4 +1,10 @@
 <x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Recipe') }}
+        </h2>
+    </x-slot>
+
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div class="grid grid-cols-1 md:grid-cols-4 gap-5">
             <div class="md:col-span-3 grid grid-cols-1 gap-5">
@@ -11,6 +17,11 @@
                             {{ $recipe->title }}
                         </div>
                         <div class="flex px-5">
+                            @can('update', $recipe)
+                                <x-button href="{{ route('recipes.edit', $recipe) }}">
+                                    {{ __('Update') }}
+                                </x-button>
+                            @endcan
                             @can('delete', $recipe)
                                 <form method="post"
                                       action="{{ route('recipes.destroy', $recipe) }}">
@@ -19,72 +30,75 @@
 
                                     <x-button component="button"
                                               type="submit"
-                                              class="mr-3"
+                                              class="ml-3"
+                                              bg="bg-red-600"
                                               onclick="return confirmDeleteModel()">
                                         {{ __('Delete') }}
                                     </x-button>
                                 </form>
                             @endcan
-                            @can('update', $recipe)
-                                <x-button href="{{ route('recipes.edit', $recipe) }}">
-                                    {{ __('Update') }}
-                                </x-button>
-                            @endcan
                         </div>
                     </div>
                 </div>
-                <div class="surface p-4">
-                    <div class="text-lg text-blue-900 font-bold">
-                        {{ __('Description') }}
-                    </div>
-                    <div>
-                        {{ $recipe->description }}
+
+                <div class="flex flex-col md:flex-row">
+                    @if($recipe->description)
+                        <div class="surface p-4 w-full md:mr-5 md:w-1/2">
+                            <div class="text-lg text-blue-900 font-bold">
+                                {{ __('Description') }}
+                            </div>
+                            <div>
+                                {!! nl2br(e($recipe->description)) !!}
+                            </div>
+                        </div>
+                    @endif
+                    <div class="surface w-full mt-5 md:mt-0 {{ $recipe->description ? 'md:w-1/2': '' }}">
+                        <div class="px-4 pt-3 pb-2 text-lg text-blue-900 font-bold">
+                            {{ __('Ingredients') }}
+                        </div>
+                        @foreach($recipe->ingredients as $ingredient)
+                            <div class="px-4 py-2 text-md border-t border-gray-300">
+                                {{ $ingredient->name }}
+                            </div>
+                        @endforeach
                     </div>
                 </div>
+
                 <div class="surface">
                     <div class="px-4 pt-3 pb-2 text-lg text-blue-900 font-bold">
                         {{ __('Instructions') }}
                     </div>
                     @foreach($recipe->instructions as $instruction)
-                        <div class="px-4 py-2 text-md border-t border-gray-300">
-                            <span class="text-blue-900 font-bold">{{ $loop->iteration }}.</span>
+                        <div class="p-4 text-md border-t border-gray-300">
+                            <span class="font-bold">{{ $loop->iteration }}.</span>
                             {{ $instruction->instruction }}
                         </div>
                     @endforeach
                 </div>
             </div>
+
             <div>
                 <div class="surface p-4">
                     <div class="text-blue-900 font-bold">
-                        Author
+                        {{ __('Author') }}
                     </div>
                     <x-user :user="$recipe->user"/>
                     <div class="text-blue-900 font-bold mt-3">
-                        Category
+                        {{ __('Category') }}
                     </div>
                     <div>
                         {{ $recipe->category }}
                     </div>
                     <div class="text-blue-900 font-bold mt-3">
-                        Bereidingstijd
+                        {{ __('Cooking time') }}
                     </div>
                     {{ \App\Services\DurationConverter::toHuman($recipe->duration) }}
                     <div class="text-blue-900 font-bold mt-3">
-                        Gepubliceerd
+                        {{ __('Published at') }}
                     </div>
                     <div>
                         {{ $recipe->created_at->format('j F, Y @ H:i') }}
                     </div>
-                </div>
-                <div class="surface mt-5">
-                    <div class="px-4 pt-3 pb-2 text-lg text-blue-900 font-bold">
-                        {{ __('Ingredients') }}
-                    </div>
-                    @foreach($recipe->ingredients as $ingredient)
-                        <div class="px-4 py-2 text-md border-t border-gray-300">
-                            {{ $ingredient->name }}
-                        </div>
-                    @endforeach
                 </div>
             </div>
         </div>
