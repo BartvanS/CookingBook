517973722938bc9cbc2025b2263d4ec328073190
Max Hoogenbosch <mhoogenbosch@enrise.com>
2021-02-20 15:38:20 +0100

    Fixed tailwindcss forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 module.exports = {
     purge: [
         './vendor/laravel/jetstream/**/*.blade.php',
@@ -11,5 +9,8 @@ module.exports = {
         opacity: ['responsive', 'hover', 'focus', 'disabled'],
     },
 
-    plugins: [require('@tailwindcss/ui')],
+    plugins: [
+        require('@tailwindcss/ui'),
+        require('@tailwindcss/forms'),
+    ],
 };
