90681653572c8b839c2a3979e07349405141680c
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-01-25 23:54:52 +0100

    Removed command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,13 +5,11 @@ declare(strict_types=1);
 namespace App\Console;
 
 use App\Console\Commands\CopyImages;
-use App\Console\Commands\UpdateImages;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 final class Kernel extends ConsoleKernel
 {
     protected $commands = [
         CopyImages::class,
-        UpdateImages::class,
     ];
 }
