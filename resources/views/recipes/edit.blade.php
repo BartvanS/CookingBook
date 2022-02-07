b8348f6761685489f197732be17a7f805c8134c0
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-02-07 19:36:01 +0100

    Added a document title

--- resources/views/recipes/edit.blade.php
+++ resources/views/recipes/edit.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-app-layout :title="__('Edit recipe')">
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Edit recipe') }}
