011cc7fb3a0b375b3be4d44959d2ba39edd75181
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-04-05 20:24:53 +0200

    Fixed tests

--- tests/Feature/DocumentTitleTest.php
+++ tests/Feature/DocumentTitleTest.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+beforeEach(function (): void {
+    config()->set('app.name', 'Laravel');
+});
+
 it('has fallback title', function (): void {
-    $this->assertEquals('Recipes', documentTitle());
+    $this->assertEquals('Laravel', documentTitle());
 });
 
 it('has page title', function (): void {
-    $this->assertEquals('Account | Recipes', documentTitle('Account'));
+    $this->assertEquals('Account | Laravel', documentTitle('Account'));
 });
