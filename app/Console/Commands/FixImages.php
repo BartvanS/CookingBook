02394c997f833aa90cd3cfa39bf63a57b33cd693
Max Hoogenbosch <mhoogenbosch@enrise.com>
2022-04-05 21:44:37 +0200

    Improved image fixer

--- app/Console/Commands/FixImages.php
+++ app/Console/Commands/FixImages.php
@@ -1,34 +1,57 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Console\Commands;
 
 use App\Models\Recipe;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
-class FixImages extends Command
+final class FixImages extends Command
 {
     protected $signature = 'image:fix';
 
     protected $description = 'Fix image paths';
 
-    public function handle()
+    public function handle(): void
     {
-        Recipe::each(function (Recipe $recipe) {
-            if (! str_starts_with( $recipe->image ?? '', 'public/')) {
-                return;
-            }
+        Recipe::each(function (Recipe $recipe): void {
+            $recipe = $this->fixImagePath($recipe);
+            $recipe = $this->fixThumbnailPath($recipe);
+            $recipe = $this->fixMissingImages($recipe);
+            $recipe->save();
+        });
+    }
 
+    public function fixMissingImages(Recipe $recipe): Recipe
+    {
+        if ($recipe->image && ! Storage::disk('recipes')->exists($recipe->image)) {
+            $recipe->image = null;
+        }
+
+        if ($recipe->thumbnail && ! Storage::disk('recipes')->exists($recipe->thumbnail)) {
+            $recipe->thumbnail = null;
+        }
+
+        return $recipe;
+    }
+
+    public function fixImagePath(Recipe $recipe): Recipe
+    {
+        if (str_starts_with($recipe->image ?? '', 'public/')) {
             $recipe->image = str_replace('public/', '', $recipe->image);
-            $recipe->save();
-        });
+        }
 
-        Recipe::each(function (Recipe $recipe) {
-            if (! str_starts_with( $recipe->thumbnail ?? '', 'public/')) {
-                return;
-            }
+        return $recipe;
+    }
 
+    public function fixThumbnailPath(Recipe $recipe): Recipe
+    {
+        if (str_starts_with($recipe->thumbnail ?? '', 'public/')) {
             $recipe->thumbnail = str_replace('public/', '', $recipe->thumbnail);
-            $recipe->save();
-        });
+        }
+
+        return $recipe;
     }
 }
--- tests/Feature/Console/Commands/FixImagesTest.php
+++ tests/Feature/Console/Commands/FixImagesTest.php
@@ -1,29 +1,22 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Console\Commands\FixImages;
 use App\Models\Recipe;
+use Illuminate\Support\Facades\Storage;
 use function Pest\Laravel\artisan;
 
-test('does not change fixed images', function () {
-    $recipe = Recipe::factory()->create([
-        'image' => 'image/image.jpg',
-        'thumbnail' => 'image/image.jpg',
-    ]);
-
-    artisan(FixImages::class)
-        ->assertSuccessful();
-
-    $recipe->refresh();
-
-    expect($recipe->image)->toBe('image/image.jpg');
-    expect($recipe->thumbnail)->toBe('image/image.jpg');
+beforeEach(function (): void {
+    Storage::fake('recipes');
+    Storage::disk('recipes')->put('image/image.jpg', 'image');
+    Storage::disk('recipes')->put('image.jpg', 'image');
 });
 
-
-test('fixed images with public prefix', function () {
+it('does not fix fixed images', function (): void {
     $recipe = Recipe::factory()->create([
-        'image' => 'public/image/image.jpg',
-        'thumbnail' => 'public/image/image.jpg',
+        'image' => 'image/image.jpg',
+        'thumbnail' => 'image.jpg',
     ]);
 
     artisan(FixImages::class)
@@ -32,5 +25,50 @@ test('fixed images with public prefix', function () {
     $recipe->refresh();
 
     expect($recipe->image)->toBe('image/image.jpg');
-    expect($recipe->thumbnail)->toBe('image/image.jpg');
+    expect($recipe->thumbnail)->toBe('image.jpg');
+});
+
+it('does not change null images', function (): void {
+    $recipe = Recipe::factory()->create([
+        'image' => null,
+        'thumbnail' => null,
+    ]);
+
+    artisan(FixImages::class)
+        ->assertSuccessful();
+
+    $recipe->refresh();
+
+    expect($recipe->image)->toBeNull();
+    expect($recipe->thumbnail)->toBeNull();
+});
+
+it('fixed images with public prefix', function (): void {
+    $recipe = Recipe::factory()->create([
+        'image' => 'public/image/image.jpg',
+        'thumbnail' => 'public/image.jpg',
+    ]);
+
+    artisan(FixImages::class)
+        ->assertSuccessful();
+
+    $recipe->refresh();
+
+    expect($recipe->image)->toBe('image/image.jpg');
+    expect($recipe->thumbnail)->toBe('image.jpg');
+});
+
+it('fixed missing images', function (): void {
+    $recipe = Recipe::factory()->create([
+        'image' => 'missing/image.jpg',
+        'thumbnail' => 'missing/image.jpg',
+    ]);
+
+    artisan(FixImages::class)
+        ->assertSuccessful();
+
+    $recipe->refresh();
+
+    expect($recipe->image)->toBeNull();
+    expect($recipe->thumbnail)->toBeNull();
 });
